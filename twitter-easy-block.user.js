3b0676763b602ff4e8f7d2eefa8afd34d304a321
HATTORI,Hiroki <seagull.kamome@gmail.com>
2020-12-07 08:05:07 +0900

    fix some bugs

--- twitter-easy-block.user.js
+++ twitter-easy-block.user.js
@@ -10,24 +10,27 @@
 // @match       https://mobile.twitter.com/*
 // @match       https://tweetdeck.twitter.com/*
 // @resource    REGEXP_ZH https://raw.githubusercontent.com/seagull-kamome/unicode-regexp/master/regexp_zh.txt
-// @resource    REGEXP_KR https://raw.githubusercontent.com/seagull-kamome/unicode-regexp/master/regexp_zhkrxt
 // @require     https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js
+// @require     https://cdn.jsdelivr.net/npm/qs/dist/qs.min.js
 // @grant       GM_getResourceText
+// @grant       GM_registerMenuCommand
 // ==/UserScript==
 (_ => {
   'use strict';
 
   const regexp_zh_str = GM_getResourceText('REGEXP_ZH');
   const regexp_zh = (regexp_zh_str === '')? null : new RegExp(regexp_zh_str, 'u');
-  const regexp_kr_str = GM_getResourceText('REGEXP_KR');
-  const regexp_kr = (regexp_kr_str === '')? null : new RegExp(regexp_kr_str, 'u');
+  const regexp_kr =  /[\u{ac00}-\u{afff}]/u;
 
   const TROLL_WORDS_REGEXP = new RegExp(
     'nmsl|[にニ][まマ][すス][らラ]|[にニ][まマ][びビ]'
     + '|マホRPGは今これをやってるよ|参戦ID.*参加者募集！'
-    + '|#(?:桐生ココ引退)'
+    + '|#(?:桐生ココ(?:引退|政治系Vtuber|はRCEP締結の元凶))'
     , 'iu');
-  const TROLL_SCREENNAME = /^(?:Jejus)$/iu;
+  const TROLL_NICKNAME = new RegExp(
+   '^(?:Jesus$|COVER豬式會社)'
+   + '|エバーライフ|バッタ殺|は引退しろ'
+   , 'iu');
   const HASHTAGS_LIMIT = 7;
   const NUM_BUDDA_FACES = 3;
 
@@ -51,44 +54,38 @@
 
   /* ********************************************************************** */
 
-  {
-    const get_cookie = k => {
-      const kk = k + '=';
-      const y = document.cookie.split(';').find(x => x.indexOf(kk) == 0);
-      return y? y.substring(kk.length, y.length) : ''; };
-    const ajax = axios.create({
-      baseURL: 'https://api.twitter.com',
-      withCredentials: true,
-      headers:
-      { 'Authorization': 'Bearer xxxxxxxxxxxx'
-        , 'X-Twitter-Auth-Type': 'OAuth2Session'
-        , 'X-Twitter-Active-User': 'yes'
-        , 'X-Csrf-Token': get_cookie('ct0') }  });
-    const blocker_cycle = _ => {
-      Object.keys(accounts)
-        .filter(k => accounts[k].typ == 'TROLL')
-        .forEach(k => ajax.post('/1.1/blocks/create.json'
-          , { user_id: id, skip_status: 1 }
-          , { headers: {
-            'Content-Type': 'application/x-www-form-urlencoded' } }) );
-      window.setTimeout(blocker_cycle, 60000); };
+  const get_cookie = k => {
+    const kk = k + '=';
+    const y = document.cookie.split(';').map(x => x.trim()).find(x => x.indexOf(kk) === 0);
+    return y? y.substring(kk.length, y.length) : ''; };
+  const ajax = axios.create({
+    baseURL: 'https://api.twitter.com',
+    withCredentials: true,
+    headers: {
+      'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
+      'X-Twitter-Auth-Type': 'OAuth2Session',
+      'X-Twitter-Active-User': 'yes',
+      'X-Csrf-Token': get_cookie('ct0') }  });
+  const block_account = async id => {
+    get_account_info(id).typ = 'BLOCKED';
+    ajax.post('/1.1/blocks/create.json',
+      Qs.stringify({ screen_name: id, skip_status: true }),
+      { headers: {
+        'Content-Type': 'application/x-www-form-urlencoded' } })};
 
-    // window.setTimeout(blocker_cycle, 60000);
-  }
+  GM_registerMenuCommand('Show trolls',
+    _ => alert(Object.keys(accounts)
+      .filter(k => accounts[k].typ === 'TROLL')
+      .join('/')) );
+  GM_registerMenuCommand('Block trolls',
+    _ => Object.keys(accounts).filter(k => accounts[k].typ === 'TROLL')
+           .forEach(block_account));
 
 
   /* ********************************************************************** */
 
-  const hide_tweet = x => {
-   const c = x.parentNode.parentNode;
-    c.style.display = 'none';
-    c.style.visibility = 'hidden';
-    x.parentNode.remove(x);
-    };
-
-  const detector = x => {
-    //
-    const elm = x.querySelector(':scope div[data-testid=\'tweet\'] > div:nth-of-type(2)');
+  const detector = article => {
+    const elm = article.querySelector(':scope div[data-testid=\'tweet\'] > div:nth-of-type(2)');
     if (!elm) return ;
 
     // Get author information.
@@ -106,24 +103,33 @@
     //console.log(author_id + ' : ' + tweet_id + ' : ' + author_name);
 
     let author_info = get_account_info(author_id);
-    if (author_info.typ == 'TROLL') { hide_tweet(x); return; }
+    article.setAttribute('tweb-processed', 'true');
+    const hide_tweet = b => {
+      if (b) author_info.typ = 'TROLL';
+      const c = article.parentNode.parentNode;
+      c.style.display = 'none';
+      c.style.visibility = 'hidden';
+      /* article.parentNode.remove(article); */ /* NOTE: removing will error occurs */ };
 
-    // Get message.
-    const msg = elm.querySelector(':scope > div:nth-of-type(2) div[lang]');
-    const lang = msg.getAttribute('lang') || '';
-    const message = (msg? msg.innerText : '').replace(/(?:\r?\n|\s)+/ig, ' ');
+    if (author_info.typ === 'TROLL' || author_info.typ === 'BLOCKED') {
+      hide_tweet(false);
+    } else {
+      // Get message.
+      const msg = elm.querySelector(':scope > div:nth-of-type(2) div[lang]');
+      const lang = msg.getAttribute('lang') || '';
+      const message = (msg? msg.innerText : '').replace(/(?:\r?\n|\s)+/ig, ' ');
 
-    if (TROLL_SCREENNAME.test(author_name) ) {
-      hide_tweet(x);
-      author_info.typ = 'TROLL';
-    } else if (TROLL_WORDS_REGEXP.test(author_name + message)
-            || msg.querySelectorAll('a[href^=\'/hashtag/\']').length > HASHTAGS_LIMIT) {
-      hide_tweet(x);
-      if (++author_info.count >= NUM_BUDDA_FACES) { author_info.typ = 'TROLL'; }
-    } else if (lang == 'zh' || lang == 'kr'
-            || (regexp_zh !== null && regexp_zh.test(author_name + message))
-            || (regexp_kr !== null && regexp_kr.test(author_name + message)) ) {
-      hide_tweet(x);
+      if (TROLL_NICKNAME.test(author_name)
+        || TROLL_WORDS_REGEXP.test(author_name)) {
+        hide_tweet(true);
+      } else if (TROLL_WORDS_REGEXP.test(message)
+              || msg.querySelectorAll('a[href^=\'/hashtag/\']').length > HASHTAGS_LIMIT) {
+        hide_tweet(++author_info.count > NUM_BUDDA_FACES);
+      } else if (lang == 'zh' || lang == 'kr'
+              || (regexp_zh !== null && regexp_zh.test(author_name + message))
+              || (regexp_kr !== null && regexp_kr.test(author_name + message)) ) {
+        hide_tweet(false);
+      }
     }
   };
 
@@ -153,7 +159,7 @@
       }
       last_processed_time = t;
 
-      twmain.querySelectorAll('article:not([visibility=\'hidden\'])')
+      twmain.querySelectorAll('article:not([tweb-processed])')
         .forEach(x => { detector(x); });
     };
 
